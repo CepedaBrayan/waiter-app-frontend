8ab26835215bf9344525cbb921e49838149465cd
Brayan Rivera Cepeda <riveracepedabrayan@gmail.com>
2022-07-12 18:49:24 -0500

    Principal component

---
+++ src/App/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <section className="container">
+        <div className="leftSection">
+          <span>Make an order</span>
+        </div>
+        <div className="rightSection">
+          <span>Print invoice</span>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
